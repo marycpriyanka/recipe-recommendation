3b8be8a652e8ecacde850da035a76275c9e053e4
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-02-01 16:14:23 -0800

    Moved app key inside the function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,25 @@
 
 let queryUrl;
-// API Id for Edamam API
-let appId = "3f548913";
-// API key for Edamam API
-let appKey = "9d558572444afeba08f4059681e7376b"
 
 // Handler for search recipe user form submission
 function formSubmitHandler(event) {
-    event.preventDefault();
+    // event.preventDefault();
+
+    // API Id for Edamam API
+    let appId = "3f548913";
+    // API key for Edamam API
+    let appKey = "9d558572444afeba08f4059681e7376b"
 
     let mainIngredient = "chicken";
     let mealType = "Lunch";
     let diet = "balanced";
     let time = "30";
 
-    queryUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=" + mainIngredient + "&app_id=" + appId + "&app_key=" + appKey 
-                + "&diet=" + diet + "&mealType=" + mealType + "&time=" + time;
+    queryUrl = "https://api.edamam.com/api/recipes/v2?type=public&q=" + mainIngredient + "&app_id=" + appId + "&app_key=" + appKey
+        + "&diet=" + diet + "&mealType=" + mealType + "&time=" + time;
     fetch(queryUrl)
         .then(function (response) {
-            if(response.ok) {
+            if (response.ok) {
                 return response.json();
             }
             else {
@@ -27,6 +28,7 @@ function formSubmitHandler(event) {
         })
         .then(function (data) {
             console.log(data);
+            console.log(data.hits[0].recipe);
             displayRecipeCards(data.hits)
         })
         .catch(function (error) {
@@ -35,5 +37,7 @@ function formSubmitHandler(event) {
 }
 
 function displayRecipeCards(recipes) {
-    
+
 }
+
+// formSubmitHandler();
